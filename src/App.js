3c9f151c56a540e4972084306c9922ca022ee527
grehashah6 <grehashah6@gmail.com>
2021-12-01 16:13:40 +0530

    login and signup page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,48 @@ import logo from "./logo.svg";
 import "./App.css";
 import LoginSide from "./pages/Login/Login";
 import SignInSide from "./pages/Signup/Signup";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom"
 
 function App() {
 	return (
-		<div className="App">
-			<LoginSide />
-      {/* <SignInSide /> */}
-		</div>
+		<Router>
+			<div className="App">
+				<Routes>
+					<Route exact path='/' element= {<LoginSide/>} />
+					<Route path='/signup' element= {<SignInSide/>} />
+					{/* <Route path='/review' element= {<Review/>} /> */}	
+				</Routes>
+			</div>
+		</Router>
 	);
+		
+
+			{/* <Router>
+<div>
+  <Link to="/">Home</Link>
+</div>
+<div>
+  <Link to="/signin">Blog Articles</Link>
+</div>
+
+<hr />
+
+<Routes>
+  <Route  path="/">
+	<LoginSide />
+  </Route>
+  <Route path="/signin">
+	< SignInSide />
+  </Route>
+</Routes>
+</Router> */}
+
+		// </div>
+
+
+// );
+// }
+// 	);
 }
 
 export default App;
