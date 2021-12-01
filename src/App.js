ba850a7d13fc90dec545f9ad549d6557bf3cded0
talkshrey <talkshrey@gmail.com>
2021-12-01 08:10:24 +0530

    Router added, review page created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
 import LoginSide from "./pages/Login/Login";
 import SignInSide from "./pages/Signup/Signup";
+import Review from "./pages/Review/Review";
+import { Route, Routes } from "react-router";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
 	return (
-		<div className="App">
-			<LoginSide />
-      {/* <SignInSide /> */}
-		</div>
+		<Router>
+			<div className="App">
+				<Routes>
+					<Route exact path='/' element= {<LoginSide/>} />
+					<Route path='/signup' element= {<SignInSide/>} />
+					<Route path='/review' element= {<Review/>} />
+				</Routes>
+			</div>
+		</Router>
 	);
 }
 
