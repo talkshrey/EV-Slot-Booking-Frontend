e8a215e1deee729c6a5fc1f58b2864d2406a87d2
talkshrey <talkshrey@gmail.com>
2022-02-07 12:13:58 +0530

    Station api called

--- src/pages/BookingPage/BookingPage.js
+++ src/pages/BookingPage/BookingPage.js
@@ -1,116 +1,55 @@
-import * as React from "react";
 import "./Booking.css";
 import { Link } from "react-router-dom";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import { Box, Rating } from "@mui/material";
+import { Box } from "@mui/material";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import CardInfo from "./CardInfo";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronCircleRight } from "@fortawesome/free-solid-svg-icons";
+import logo from "../../assets/images/LPlogo.png";
+import { useEffect, useState } from "react";
 
 export default function Booking() {
-	return (
-		<Box>
-			{/* <img
-				src="https://drive.google.com/uc?export=download&id=1nugV0IRHoEz8iqO7Kx80NgmmRze0oCRM"
-				style={{
-					marginTop: "25px",
-					display: "flex",
-					justifyContent: "flex-start",
-					marginLeft: "25px",
-				}}
-				className="bp_logo"
-			/>
-			<div className="headers">
-				<Link to="/book" className="title">
-					{" "}
-					<div> Book </div>{" "}
-				</Link>
-				<Link to="/review" className="title">
-					{" "}
-					<div> Reviews </div>{" "}
-				</Link>
-				<Link to="/payment" className="title">
-					{" "}
-					<div> Payment </div>{" "}
-				</Link>
-			</div> */}
+	const [data, setData] = useState([])
+	useEffect(() => {
+		var myHeaders = new Headers();
+		myHeaders.append(
+		  "Authorization",
+		  `Token ${localStorage.getItem('token')}`
+		);
+		myHeaders.append(
+		  "Cookie",
+		  "csrftoken=PNWvCigcHKd01ul44FUpyLNwLbkVZTJNHv4NtPEwmhnedricHyK02uduZJy3Uump"
+		);
+	
+		var requestOptions = {
+		  method: "GET",
+		  headers: myHeaders,
+		  redirect: "follow",
+		};
+	
+		fetch("https://findmyplug.herokuapp.com/station/", requestOptions)
+		  .then((response) => response.json())
+		  .then((result) => {
+			console.log(result)
+			setData(result)
+		  })
+		  .catch((error) => console.log("error", error));
+	  }, []);
 
 
-<div className="nav-bar">
-                <div className="logo"> Logo </div>
+  return (
+    <Box>
+      <div className="nav-bar">
+                <div className="logo"> <img src={logo} alt="logo"/> </div>
                 <div className="headers">
-                    <Link to="/book" className="title"> <div> Book </div> </Link>  
-                    <Link to="/review" className="title"> <div> Review </div> </Link> 
+                    <Link to="/booking" className="title"> <div> Book </div> </Link>  
+                    <Link to="/review" className="title"> <div> Reviews </div> </Link> 
                     <Link to="/payment" className="title"> <div> Payment </div> </Link> 
                 </div>
             </div>
 
-
-			{/* <Typography>Book</Typography>
-			<Typography>Review</Typography> */}
-
-			<Grid container spacing={2}>
-				<Grid item xs={12}></Grid>
-				<Grid item xs={6} sm={1}>
-					<Paper></Paper>
-				</Grid>
-				<Grid item xs={6} sm={2}>
-					<Paper>
-						<CardInfo />
-					</Paper>
-				</Grid>
-				<Grid item xs={6} sm={2}>
-					<Paper>
-						<CardInfo />
-					</Paper>
-				</Grid>
-				<Grid item xs={6} sm={2}>
-					<Paper>
-						<CardInfo />
-					</Paper>
-				</Grid>
-				<Grid item xs={6} sm={2}>
-					<Paper>
-						<CardInfo />
-					</Paper>
-				</Grid>
-				<Grid item xs={6} sm={3}>
-					<FontAwesomeIcon icon={faChevronCircleRight} cursor="pointer" className="right_arrow" />
-				</Grid>
-				<Grid item xs={6} sm={1}>
-					<Paper></Paper>
-				</Grid>
-				<Grid item xs={6} sm={2}>
-					<Paper>
-						<CardInfo />
-					</Paper>
-				</Grid>
-				<Grid item xs={6} sm={2}>
-					<Paper>
-						<CardInfo />
-					</Paper>
-				</Grid>
-				<Grid item xs={6} sm={2}>
-					<Paper>
-						<CardInfo />
-					</Paper>
-				</Grid>
-				<Grid item xs={6} sm={2}>
-					<Paper>
-						<CardInfo />
-					</Paper>
-				</Grid>
-				{/* <Grid item xs={6} sm={2}>
-				<FontAwesomeIcon icon={faChevronCircleRight} cursor="pointer" className="right_arrow" />
-				</Grid> */}
-			</Grid>
-		</Box>
-	);
+      <Grid container spacing={2}>
+		  {data.map((item, index)=> <Grid item xs={3} key={index}> <Paper> <CardInfo name={item.station_name} address={item.location} number={item.phone_no} value={item.star_rating}/> </Paper>  </Grid> )}
+      </Grid>
+    </Box>
+  );
 }
